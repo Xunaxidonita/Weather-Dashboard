fcfde6f9619d8b65948ec8c85448763a885b285f
Maria Donaji Velazquez <xunaxidonaji@hotmail.com>
2021-03-08 17:44:37 -0800

    the HTML has the basic structure to start ading data from API

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const WEATHER_URL =
+  "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=";
+const API_KEY = "0564c081313fe54741f1983cf3dea38c";
+var form = document.getElementById("search-word");
